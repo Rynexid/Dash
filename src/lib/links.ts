bf4c39821968dd81fa954bcb44b7625869b767a2
Riu <echo.soraku@gmail.com>
2026-09-09 09:50:06 +0700

    Update bot invite permissions with voice moderation

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,4 +1,5 @@
-const RYNOTE_INVITE = "https://discord.com/oauth2/authorize?client_id=1496804643530080376&permissions=4855449856833872&integration_type=0&scope=bot+applications.commands"
+const RYNOTE_INVITE =
+  "https://discord.com/oauth2/authorize?client_id=1496804643530080376&permissions=687261216064&response_type=code&redirect_uri=https%3A%2F%2Frynote.apps.bot-hosting.cloud%2Fapi%2Fauth%2Fcallback%2Fdiscord&integration_type=0&scope=identify+email+guilds+bot+applications.commands"
 const SORAKU_INVITE = "https://discord.gg/MsxdNeExdg"
 const GITHUB = "https://github.com/Rynexid"
 
